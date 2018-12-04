c2da3b99b0640d7f3b29e334004e6cf40e5c687d
maninder singh <manindersingh221@gmail.com>
2018-12-04 12:09:52 +0530

    added edittodo snapshot

--- src/editTodo.spec.js
+++ src/editTodo.spec.js
@@ -4,7 +4,7 @@ import EditTodo from './Todo/EditTodo';
 describe('EditTodo', () => {
     let wrapper;
     beforeEach(() => wrapper = shallow(<EditTodo editText={""} handleUpdateTodo={jest.fn()} />));
-
+    it('should render correctly', () => expect(wrapper).toMatchSnapshot());
     it('should render a <div />', () => {
         expect(wrapper.find('div').length).toBeGreaterThanOrEqual(1);
     });
